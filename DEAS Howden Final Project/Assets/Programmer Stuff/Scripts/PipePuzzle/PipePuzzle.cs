9eaaa27835f7948820f0aec144195df8f175f5a8
Ubitroll <stephencartner@hotmail.co.uk>
2019-12-10 09:34:52 +0000

    Ai script moved over

---
+++ DEAS Howden Final Project/Assets/Programmer Stuff/Scripts/PipePuzzle/PipePuzzle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipePuzzle : MonoBehaviour
+{
+    public int numSelectors = 5;
+    public GameObject[] selectorArr;
+    public GameObject selector; //selected in the editor
+    void Start()
+    {
+        selectorArr = new GameObject[numSelectors];
+        for (int i = 0; i < numSelectors; i++)
+        {
+            GameObject go = Instantiate(selector, new Vector3((float)i, 1, 0), Quaternion.identity) as GameObject;
+            go.transform.localScale = Vector3.one;
+            selectorArr[i] = go;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ DEAS Howden Final Project/Assets/Programmer Stuff/Scripts/Stephen AI Scripts/SimpleAI.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SimpleAI : MonoBehaviour
+{
+    //Variables
+    public GameObject desk;
+    public GameObject machine;
+
+    // AI States
+    protected enum state
+    {
+        Working,
+        Fixing
+    }
+
+    // Time it takes to fix a machine
+    private const float angerTime = 5f;
+    private const float fixTime = 15f;
+
+    // Time untill moveing
+    private float timeLeftUntilFix = angerTime;
+    private float timeLeftUntilWork = fixTime;
+
+    // Denotes the current state
+    private state currentState;
+
+    // Navmesh agent
+    public NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = state.Working;
+        agent.SetDestination(desk.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentState == state.Working)
+        {
+            float distanceToDesk = (desk.transform.position - this.transform.position).magnitude;
+            if (distanceToDesk <= 4)
+            {
+                if (machine.GetComponent<Machine>().currentState == Machine.MachineState.BROKEN)
+                {
+
+
+                    timeLeftUntilFix -= Time.deltaTime;
+                    if (timeLeftUntilFix <= 0.0f)
+                    {
+                        timeLeftUntilFix = angerTime;
+                    }
+
+                    if (timeLeftUntilFix == angerTime)
+                    {
+
+                        agent.SetDestination(machine.transform.position);
+                        agent.stoppingDistance = 3f;
+                        currentState = state.Fixing;
+                    }
+                }
+            }
+        }
+
+        if (currentState == state.Fixing)
+        {
+            float distanceToMachine = (machine.transform.position - this.transform.position).magnitude;
+            if (distanceToMachine <= 4)
+            {
+                timeLeftUntilWork -= Time.deltaTime;
+                if (timeLeftUntilWork <= 0.0f)
+                {
+                    timeLeftUntilWork = fixTime;
+                }
+
+                if (timeLeftUntilWork == fixTime)
+                {
+                    machine.GetComponent<Machine>().currentState = Machine.MachineState.WORKING;
+                    machine.GetComponent<Machine>().timer.timeLeft = Random.Range(8, 16);
+                    agent.SetDestination(desk.transform.position);
+                    currentState = state.Working;
+                }
+            }
+        }
+        
+
+    }
+}
