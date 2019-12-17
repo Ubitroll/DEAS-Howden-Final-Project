8afa5161a9501e0b45ce62f80039eaa9e3d12dd6
DylanWeir27 <56024593+DylanWeir27@users.noreply.github.com>
2019-12-17 03:18:48 +0000

    new cam

--- Assets/Programmer Stuff/Scripts/Puzzle room/PuzzleRoomRandomSpawn.cs
+++ Assets/Programmer Stuff/Scripts/Puzzle room/PuzzleRoomRandomSpawn.cs
@@ -17,17 +17,17 @@ public class PuzzleRoomRandomSpawn : MonoBehaviour
     {
 
 
-        room = UnityEngine.Random.Range(0, 4);  //randomly chooses between all 5 room prefabs
+        room = UnityEngine.Random.Range(0, 3);  //randomly chooses between all 5 room prefabs
         
         if (prevRoom == room)   //makes sure it wont spawn the same room twice in a row
         {
-            room = UnityEngine.Random.Range(0, 4);
-            Instantiate(Rooms[room], new Vector3(0, 0, 0), Quaternion.identity);
+            room = UnityEngine.Random.Range(0, 3);
+            Instantiate(Rooms[room], new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z), Quaternion.identity);
         }
         else
-            Instantiate(Rooms[room], new Vector3(0, 0, 0), Quaternion.identity);    //spawns the randomly chosen room
+            Instantiate(Rooms[room], new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z), Quaternion.identity);    //spawns the randomly chosen room
 
-        room = prevRoom;
+        prevRoom = room;
 
     }
 
