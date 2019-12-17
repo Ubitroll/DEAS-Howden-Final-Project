de6c9d258244a037a320e2179f94c75e163838a6
DylanWeir27 <56024593+DylanWeir27@users.noreply.github.com>
2019-12-17 03:59:07 +0000

    Revert "fixed time"
    This reverts commit 1de0ad235ab345b66768788150a2ad702dd2756c.

--- Assets/Programmer Stuff/Scripts/Ui/ScoreScript.cs
+++ Assets/Programmer Stuff/Scripts/Ui/ScoreScript.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class ScoreScript : MonoBehaviour
 {
     float minutes;
-    int hours;    
-  
+    int hours;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -17,17 +23,19 @@ public class ScoreScript : MonoBehaviour
 
     public void GameClock()
     {
-
-        minutes += Time.deltaTime;
-
-        if (minutes >= 59)
+        
+        
+        if (minutes > 59)
         {
-            hours += 1;
+            hours =+ 1;
             minutes = 0;
-        }      
-
+        }
+        else
+        {
+            minutes += Time.deltaTime;
+        }
         GameData.minutes = minutes;
-        GameData.hours = hours + 9;
+        GameData.hours = hours;
     }
 
 }
